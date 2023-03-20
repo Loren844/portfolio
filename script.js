8aed055db9b654543d6c4edb7814bf3a3d768090
ryudoy <79362075+Loren844@users.noreply.github.com>
2023-03-20 18:46:27 +0100

    modifs rapides

--- script.js
+++ script.js
@@ -22,14 +22,14 @@ const commands = [
     }
     
     function downloadCV() {
-        window.open('mon_cv.pdf');
+        window.open('CV_Lorenzo_BALADIER.pdf');
         clearConsole();
     }
     
     function displayProjects() {
         let projectList = '<p>Cliquez sur le projet qui vous intéresse:<br><ol>';
         projectList += '<li><a href="https://github.com/Loren844/LOTO-SAE" target="_blank">Jeu de loto en C</a></li>';
-        projectList += '<li><a href="#">Jeu "FISK" (java), variante de Risk</a></li>';
+        projectList += '<li><a href="#">Jeu "FISK" (java), variante de Risk (en cours)</a></li>';
         projectList += '</ol>';
         output.innerHTML += projectList;
     }
@@ -40,13 +40,13 @@ const commands = [
 
     // Script pour la fonction "contact"
     function contact() {
-        var message = 'Vous pouvez me contacter par les moyens ci-dessous :<br><br>Mail : lobaladier@gmail.com <br>Tel : +33 7 62 07 67 65<br><a href="https://www.linkedin.com/a remplir" target="_blank">Voir mon LinkedIn</a>';
+        var message = 'Vous pouvez me contacter par les moyens ci-dessous :<br><br>Mail : lobaladier@gmail.com <br>Tel : +33 7 62 07 67 65<br><a href="https://www.linkedin.com/in/lorenzo-baladier/" target="_blank">Voir mon LinkedIn</a>';
         output.innerHTML = message;
     }
     
     // Script pour la fonction "à propos"
     function about() {
-        var message = "A modifier...";
+        var message = "Jeune étudiant passionné d'informatique , je rêve de pouvoir vivre de cette passion. J'ai encore beaucoup à apprendre, c'est pourquoi je souhaite intégrer un parcours qui m'aidera à atteindre mon objectif ! ";
         output.innerHTML = message;
     }
     
