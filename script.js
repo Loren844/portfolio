bac5b0a2b7d8127217fee995daa23d3b7c70c017
Loren844 <79362075+Loren844@users.noreply.github.com>
2024-06-03 16:21:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const userInput = document.getElementById('userInput');
 const output = document.getElementById('output');
+const closeButton = document.getElementById('closeButton');
+const quitButton = document.getElementById('quitButton');
 
 const commands = [
 	{command: '/help', description: 'Afficher la liste des commandes disponibles'},
 	{command: '/cv', description: 'T\u00E9l\u00E9charger mon CV'},
 	{command: '/projects', description: 'Afficher mes projets'},
     {command: "/contact", description: "Pour me contacter / Voir mes liens utiles"},
-    {command: "/lorenzo", description: "En savoir plus sur moi"},
-    {command: "/softskills", description:"Découvrir les softs skills que j'ai acquis"},
-    {command: "/education", description: "Voir mon parcours scolaire et professionnel"},
-    {command: "/languages", description: "Voir les langages/frameworks que je connais"},
+    {command: "/aboutme", description: "En savoir plus sur moi"},
+    {command: "/skills", description:"Découvrir mes compétences"},
+    {command: "/education", description: "Afficher mon parcours scolaire"},
+    {command: "/work", description: "Afficher mes expériences professionnelles"},
     {command: "/dark", description: "Passer au th&egrave;me sombre"},
     {command: "/light", description: "Passer au th&egrave;me clair"},
-    {command: "/snake", description: "Jouer à un snake codé par moi-même"},
+    {command: "/snake", description: "Jouer à un jeu snake codé par moi-même"},
     {command: 'clear', description: 'Effacer la console'}]
-    //rajouter les softs skills, presentation entreprise
 
-    const commandsText = ['/help', '/cv', '/projects', '/contact', '/lorenzo', '/softskills', '/education', '/languages', '/dark', '/light', '/snake', 'clear'];
+    const commandsText = ['/help', '/cv', '/projects', '/contact', '/aboutme', '/skills', '/education', '/work', '/dark', '/light', '/snake', 'clear'];
 
     document.getElementById('enterButton').addEventListener('click', function() {
         document.getElementById('curtain').style.transform = 'translateY(-100%)';
@@ -70,8 +71,9 @@ const commands = [
     
     function displayProjects() {
         let projectList = '<p>Cliquez sur le projet qui vous int&eacute;resse:<br><ol>';
-        projectList += '<li><a href="https://github.com/Loren844/LOTO-SAE" target="_blank">Jeu de loto en C - Semestre 1</a></li>';
-        projectList += '<li><a href="https://loren844.github.io/FISK/" target="_blank">Jeu "FISK" (java), variante de Risk - Semestre 2</a></li>';
+        projectList += '<li><a href="https://github.com/Loren844/LOTO-SAE" target="_blank">Jeu de loto en C - Semestre 1 BUT Informatique</a></li>';
+        projectList += '<li><a href="https://loren844.github.io/FISK/" target="_blank">Jeu "FISK" (java), variante de Risk - Semestre 2 BUT Informatique</a></li>';
+        projectList += '<li><a href="https://github.com/Loren844/filrouge" target="_blank">Projet Application Mobile - Semestre 4 BUT Informatique</a></li>';
         projectList += '<li><a href="https://github.com/Loren844/blorenzo-portfolio" target="_blank">Portfolio personnel</a></li>';
         projectList += '</ol>';
         output.innerHTML += projectList;
@@ -83,65 +85,100 @@ const commands = [
 
     // Script pour la fonction "contact"
     function contact() {
-        var message = 'Vous pouvez me contacter par les moyens ci-dessous :<br><br>Mail : lobaladier@gmail.com <br>Tel : +33 7 62 07 67 65<br><a href="https://www.linkedin.com/in/lorenzo-baladier/" target="_blank">Voir mon LinkedIn</a><br><a href="https://github.com/Loren844" target="_blank">Voir mon GitHub</a>';
+        var message = 'Vous pouvez me contacter par les moyens ci-dessous :' +
+        '<br><br>Mail : lobaladier@gmail.com <br>' + 
+        'Tel : +33 7 62 07 67 65<br>' +
+        '<a href="https://www.linkedin.com/in/lorenzo-baladier/" target="_blank">Voir mon LinkedIn</a><br>' +
+        '<a href="https://github.com/Loren844" target="_blank">Voir mon GitHub</a>';
         output.innerHTML = message;
     }
     
     // Script pour la fonction "à propos"
     function about() {
-        var message = "Ma passion pour le code me vient d'une passion encore plus forte : celle des jeux vidéo. J'ai commencé à coder en 2019, lorsque j'avais 14 ans en ayant comme projet de créer un jeu. <br><br>Après avoir un peu codé en <u>Python</u>, j'ai rapidement su que c'était fait pour moi. C'est pourquoi j'ai choisi la spécialité NSI au lycée, grâce à laquelle j'ai pu consolider mes bases de <u>Python</u> ainsi que de commencer le <u>SQL</u>, le <u>HTML</u> ainsi que le <u>CSS</u>. <br><br>Après l'obtention de mon baccalauréat, il m'est paru naturel de continuer dans une filière d'informatique. Grâce au BUT Informatique, j'ai pu apprendre <u>le C</u>, et je commence <u>le C++</u>. Lors de mes semaines en entreprise j'ai du me former au <u>Java</u> ainsi qu'au <u>TypeScript</u>, car j'ai pour projet d'automatiser un site web (au début en <u>Selenium</u> puis basculement sur du <u>Cypress</u>). <br><br>J'ai aussi l'occasion d'apprendre le <u>Bash</u> et de m'améliorer en <u>SQL</u>. J'ai également des cours de développement Web qui me permettent d'approfondir mes connaissances en <u>HTML</u> et <u>CSS</u> ainsi que de découvrir le <u>JavaScript</u>.";
+        var message = "Je suis actuellement étudiant en BUT Informatique en alternance depuis deux ans. Cette formation m'a permis de combiner des connaissances théoriques solides avec une expérience pratique en entreprise, ce qui m'a donné une compréhension approfondie des enjeux et des technologies du secteur informatique.<br><br>" +
+        "En septembre, je vais débuter un cycle d'ingénieur en informatique à Polytech Nice Sophia, également en alternance. Cette nouvelle étape représente une formidable opportunité pour approfondir mes compétences techniques et renforcer mon expertise, tout en continuant à appliquer ces connaissances dans un cadre professionnel.<br><br>" + 
+        "Passionné par le développement de solutions innovantes et l'optimisation des systèmes informatiques, je suis déterminé à contribuer de manière significative aux projets auxquels je participe. Mon parcours en alternance m'a inculqué des valeurs de rigueur, d'adaptabilité et de travail en équipe, des atouts que je compte bien mettre à profit dans mes futures missions.";
         output.innerHTML = message;
     }
 
-    function softskills(){
-        let softskillsList = '<p>Voici mes diff&eacute;rents Softs Skills :<br><ol>';
-        softskillsList += '<li><p><b>Gestion du stress</b> : Je suis quelqu’un de très calme, ce qui me permet de rester concentré même dans les situations les plus stressantes. Je suis capable de prendre du recul et de réfléchir à la situation pour trouver une solution.</li><br>'
-        softskillsList += '<li><p><b>Réaliste</b> mais ambitieux : Bien que je sois très réaliste et rationnel sur mon travail, je suis quelqu’un qui voit les choses en grand et qui ne les fait que très rarement à moitié (lorsque le temps nous manque). Je ne recule pas devant le moindre obstacle et je peux persévérer des heures même si je n’en vois pas le bout.</li><br>'
-        softskillsList += '<li><p><b>Travail en équipe</b> : Je suis quelqu’un de plutôt calme, ce qui me permet d’écouter mes coéquipiers sans vouloir en faire qu’à ma tête.</li><br>'
-        softskillsList += '<li><p><b>Indépendance</b> : J’aime me retrouver seul pour me ressourcer et travailler de manière indépendante me fait du bien. J’arrive à me gérer sans avoir besoin d\'une supervision constante. Le fait d’être seul me permet de me concentrer davantage.</li><br>'
-        softskillsList += '<li><p><b>Adaptabilité</b> : Mon calme fait que je ne suis que très peu affecté par mes émotions, ce qui me permet de travailler sous pression efficacement. Bien que j’ai des préférences, je peux m’adapter à tout. Que cela soit en termes de coéquipiers, de tâches…</li>'
-        softskillsList += '<li><p><b>Curieux</b> : Lorsqu’un sujet me passionne, je suis très curieux d’en apprendre plus et très enthousiaste de sortir des sentiers battus. Évidemment comme tout le monde j’ai des préférences, mais découvrir un nouvel aspect d’un sujet n’est jamais une perte de temps pour moi.</li><br>'
-        output.innerHTML += softskillsList;
-    }
-
-    function languages() {
+    // Script pour la fonction "skills"
+    function skills() {
         var message =
-        "<span style=\"color: cyan;\">Langages</span><br>" +
-        "├── <span style=\"color: red;\">Back end</span><br>"+
-        "│   ├── <span style=\"color: blue;\">Orienté objet</span><br>" + 
-        "│   │   ├── Java ****<br>" +
-        "│   │   └── C++ **<br>"+
-        "│   ├── Python ****<br>"+
-        "│   ├── C ***<br>"+
-        "│   ├── SQL **<br>"+
-        "│   └── Bash *<br>"+
-        "├── <span style=\"color: green;\">Front end</span><br>"+
-        "│   ├── HTML ***<br>"+
-        "│   ├── CSS ***<br>"+
-        "│   ├── JavaScript **<br>"+
-        "│   └── TypeScript **<br>"+
-        "└── <span style=\"color: magenta;\">Frameworks</span><br>"+
-        "    ├── Cypress ***<br>"+
-        "    ├── Selenium ***<br>"+
-        "    └── TestNG ***<br></pre>";
+        "<pre>├── hard-skills<br>" +
+        "│   ├── programming-languages<br>" +
+        "│   │   ├── Java                [<span style=\"color: green;\">██████████████████</span>..] 90%<br>" +
+        "│   │   ├── Python              [<span style=\"color: green;\">████████████████</span>....] 80%<br>" +
+        "│   │   ├── C/C++               [<span style=\"color: green;\">████████████</span>........] 60%<br>" +
+        "│   │   ├── JavaScript/TS       [<span style=\"color: green;\">████████████</span>........] 60%<br>" +
+        "│   │   ├── SQL                 [<span style=\"color: green;\">████████████</span>........] 60%<br>" +
+        "│   │   └── PHP                 [<span style=\"color: orange;\">████████</span>............] 40%<br>" +
+        "│   ├── web-development<br>" +
+        "│   │   ├── HTML                [<span style=\"color: green;\">██████████████</span>......] 70%<br>" +
+        "│   │   ├── CSS                 [<span style=\"color: green;\">██████████████</span>......] 70%<br>" +
+        "│   │   ├── NodeJS              [<span style=\"color: orange;\">██████████</span>..........] 50%<br>" +
+        "│   │   ├── Bootstrap           [<span style=\"color: orange;\">██████████</span>..........] 50%<br>" +
+        "│   │   ├── TailWind            [<span style=\"color: orange;\">████████</span>............] 40%<br>" +
+        "│   │   ├── Angular             [<span style=\"color: orange;\">████████</span>............] 40%<br>" +
+        "│   │   └── ViteJS              [<span style=\"color: red;\">██████</span>..............] 30%<br>" +
+        "│   ├── tools-and-frameworks<br>" +
+        "│   │   ├── Git                 [<span style=\"color: orange;\">██████████</span>..........] 50%<br>" +
+        "│   │   ├── Maven               [<span style=\"color: orange;\">████████</span>............] 40%<br>" +
+        "│   │   ├── Spring              [<span style=\"color: red;\">██████</span>..............] 30%<br>" +
+        "│   │   ├── Bash                [<span style=\"color: red;\">██████</span>..............] 30%<br>" +
+        "│   │   └── Docker              [<span style=\"color: red;\">████</span>................] 20%<br>" +
+        "│<br>" +
+        "└── soft-skills<br>" +
+        "    ├── Autonomie<br>" +
+        "    ├── Gestion du stress<br>" +
+        "    ├── Travail en équipe<br>" +
+        "    └── Adaptabilité<pre>";
         output.innerHTML = message;
     }
     
     function education() {
-        var message = "Je suis actuellement &eacute;tudiant &agrave; l'IUT Nice C&ocirc;te d'Azur, sur le p&ocirc;le de Sophia-Antipolis, afin de poursuivre une formation en 3 ans intitul&eacute;e BUT Informatique. Je poursuis cette formation en alternance, dans l'entreprise ESI (le Cannet), ce qui me permet de me familiariser avec le monde du travail. <br><br>J'ai intégré cette entreprise en tant qu'alternant tests et validation, ma mission principale chez ESI est de mettre en place un processus d'automatisation des tests des logiciels développés afin de réduire la charge de travail des QA Testers. <br><br> J'ai commencé par coder les tests en Java avec Selenium, cependant nous nous sommes rendus compte que pour la mise en place d'une chaîne d'intégration complète cette solution n'était pas la plus viable. C'est pourquoi nous avons choisi d'utiliser Cypress avec TypeScript.";
+        var message = "<ul>" +
+                      "<li><b>Cycle d'ingénieur en informatique - 2024/2027</b><br>" +
+                      "Polytech - Nice Sophia</li><br><br>" +
+                      "<li><b>BUT Informatique en alternance - 2022/2024</b><br>" +
+                      "IUT Nice Côte d’Azur - Site de Sophia Antipolis</li><br><br>" +
+                      "<li><b>Baccalauréat Général, Section ESABAC, Mention Très Bien - 2019/2022</b><br>" +
+                      "Lycée Pierre et Marie Curie - Menton</li><br><br>" +
+                      "</ul>";
         output.innerHTML = message;
     }
 
-     function toggleLightTheme() {
+    function workexperience() {
+        var message = "<ul>" +
+                        /*
+                        "<li><b>Alternant Ingénieur développement logiciel - 2024/2027</b><br>" +
+                        "Ampere Software Technology (Renault Group) - Valbonne<br>" +
+                        "<ul>" +
+                            "<li>Analyses de performances</li>" +
+                            "<li>Développement C++ / Rust</li>" +
+                        "</ul>" +
+                        "</li><br>" +
+                         */
+                        "<li><b>Alternant Développeur - 2022/2024</b><br>" +
+                        "ESI - Le Cannet<br>" +
+                        "<ul>" +
+                            "<li>Développement d'un éditeur de bannières (Drag & Drop) en TypeScript avec Angular</li>" +
+                            "<li>Développement d’une application JAVA d’export de données au format JSON depuis une base de données distante</li>" +
+                            "<li>Développement de tests automatisés en Java grâce à Selenium et TestNG puis basculement sur du TypeScript avec Cypress</li>" +
+                        "</ul>" +
+                        "</li>" +
+                        "</ul>";
+        output.innerHTML = message;
+    }
+
+    function toggleLightTheme() {
         const body = document.querySelector('body');
         body.classList.replace('theme-dark', 'theme-light');
-        clearConsole();
      }
   
      function toggleDarkTheme() {
         const body = document.querySelector('body');
         body.classList.replace('theme-light', 'theme-dark');
-        clearConsole();
+
      }
 
      function snakeGame(){
@@ -162,6 +199,20 @@ const commands = [
         direction = 'up';
         main()
      }
+
+    function closeterminal() {
+        document.getElementById('curtain').style.transform = 'translateY(0)';
+    }
+
+    function quitTerminal(){
+        document.getElementById('terminal').style.display = 'none';
+        document.getElementById('easter-egg').style.display = 'block';
+    }
+
+    closeButton.addEventListener('click', closeterminal);
+
+    quitButton.addEventListener('click', quitTerminal);
+
 
     userInput.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
@@ -191,21 +242,21 @@ const commands = [
                     clearConsole();
                     contact();
                     break;
-                case '/lorenzo':
+                case '/aboutme':
                     clearConsole();
                     about();
                     break;
-                case '/softskills':
+                case '/skills':
                     clearConsole();
-                    softskills();
+                    skills();
                     break;
                 case '/education':
                     clearConsole();
                     education();
                     break;
-                case '/languages':
+                case '/work':
                     clearConsole();
-                    languages();
+                    workexperience();
                     break;
                 case '/light':
                     toggleLightTheme();
